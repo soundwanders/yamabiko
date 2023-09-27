724d47f739e51a7b394766bc1253f464b0d2a097
soundwanders <soundwanders@gmail.com>
2023-09-26 21:48:50 -0400

    Light/Dark Theme Modes

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,17 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let currentTheme = "{{ data.theme }}"; 
+    let currentTheme = localStorage.getItem("theme") || "{{ data.theme }}";
 
     const themeToggle = document.getElementById("theme-toggle");
     const themeLink = document.querySelector("link[rel='stylesheet']");
+    const root = document.documentElement; // Get the root element to access CSS variables
 
-    // Toggle between light and dark themes
-    themeToggle.addEventListener("click", () => {
-        const newTheme = currentTheme === "light" ? "dark" : "light";
-        
+    // Function to toggle the theme
+    function toggleTheme(newTheme) {
         themeLink.href = `/assets/css/${newTheme}-theme.css`;
+        root.style.setProperty('--bg', `var(--${newTheme}-bg)`); // Update background color
+        root.style.setProperty('--text', `var(--${newTheme}-text)`); // Update text color
         currentTheme = newTheme; // Reassign currentTheme
 
         // Save the user's preference in localStorage (could also use cookies)
         localStorage.setItem("theme", currentTheme);
+    }
+
+    // Check the user's preference from localStorage
+    if (currentTheme === "dark") {
+        toggleTheme("dark");
+    } else {
+        toggleTheme("light");
+    }
+
+    // Toggle between light and dark themes
+    themeToggle.addEventListener("click", () => {
+        const newTheme = currentTheme === "light" ? "dark" : "light";
+        toggleTheme(newTheme);
     });
 });
--- posts/2023-09-24-sudden-arrival.md
+++ posts/2023-09-24-sudden-arrival.md
@@ -12,6 +12,6 @@ tags:
 
 # Arrival and revival from the Riviera Maya
 
-Spent a week in <span style="color:#de768a">Mexico</span>, enjoyed some beautiful weather while swimming in the Caribbean ocean, visiting some Mayan ruins, and eating a lot of great food. Back home in New England, it's a long ways away from 95 and sunny. Today I have been working on implementing a light/dark theme toggle button to give the user (that's you) better control over the color scheme.
+Spent a week in <span style="color:#de768a">Mexico</span>, enjoyed some beautiful weather while swimming in the Caribbean ocean, visited some Mayan ruins, and devoured a lot of great food. Back home, a long way away from 95 and sunny, it's time to get back to work. Today, I have been working on implementing a light/dark theme toggle button to give the user (that's you) better control over the color scheme.
 
 *fin* ᓚᘏᗢ
